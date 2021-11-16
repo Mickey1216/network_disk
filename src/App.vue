b9a4df42f1ca5ede1a3423d58097d10e651f0f92
liululu <383449174@qq.com>
2021-11-16 20:09:50 +0800

    environment setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Nav/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios"
+import Nav from './components/Nav'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  components:{
+    Nav
+  },
+  methods:{
+    async getData(){
+      const result = await axios.get('/user')
+      console.log(result)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>hello vue</h1>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+    }
+  },
+  methods() {}
+}
+</script>
+<style lang="scss" scoped>
+</style>
